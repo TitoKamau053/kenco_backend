71fcb09662d42bbcb2cb0eb39d1acd4ffdc4255f
Titus Maina Kamau <117713706+TitoKamau053@users.noreply.github.com>
2025-06-08 20:09:05 +0300

    Delete migrations directory

--- migrations/seed/003_seed_tenants.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tenants (user_id, property_id, rent_amount, lease_start, lease_end, deposit_amount) VALUES
-(2, 2, 120000.00, '2023-12-01', '2024-11-30', 120000.00),
-(3, 4, 25000.00, '2023-11-01', '2024-10-31', 25000.00);
